a545c4b481d1075efa93cd2f362e12acae39b4d1
unknown <lukatassano070@gmail.com>
2019-09-22 01:08:40 -0300

    feature send email when delete

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e0046dc7c32f43',
+    pass: '2ac0771ac84eb3',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
